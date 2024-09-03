6f960fcf22165f5f2801bf4803a8b4cb0fe39fac
raguraman <raguraman.mps6066@gmail.com>
2024-09-03 14:19:07 +0530

    Todo added implemented

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import "./App.css";
-
+import NewTodoForm from "./components/NewTodoForm";
+import TodoTable from "./components/TodoTable";
+import React, { useState } from "react";
 function App() {
+  const [todos, setTodos] = useState([
+    { rowNumber: 1, rowDescription: "Feed puppy", rowAssigned: "User one" },
+    { rowNumber: 2, rowDescription: "Water plants", rowAssigned: "User two" },
+    { rowNumber: 3, rowDescription: "Make dinner", rowAssigned: "User three" },
+    {
+      rowNumber: 4,
+      rowDescription: "Charge phone battery",
+      rowAssigned: "User four",
+    },
+  ]);
+
+  const addTodo = (description, assigned) => {
+    if (todos.length > 0) {
+      const newTodo = {
+        rowNumber: todos.length + 1,
+        rowDescription: description,
+        rowAssigned: assigned,
+      };
+      setTodos((todos) => [...todos, newTodo]);
+    }
+  };
   return (
-    <div className="App">
-      <p>This is my first react project</p>
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">Your Todo's</div>
+        <div className="card-body">
+          <TodoTable todos={todos} />
+          <button className="btn btn-primary" onClick={addTodo}>
+            Add new todo
+          </button>
+
+          <NewTodoForm addTodo={addTodo} />
+        </div>
+      </div>
     </div>
   );
 }
