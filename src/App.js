bd058330534384160168aaefa63be3e5842c3980
raguraman <raguraman.mps6066@gmail.com>
2024-09-03 16:10:22 +0530

    complete todo app with state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import NewTodoForm from "./components/NewTodoForm";
 import TodoTable from "./components/TodoTable";
 import React, { useState } from "react";
 function App() {
+  const [showAddTodoForm, setShowAddTodoForm] = useState(false);
+
   const [todos, setTodos] = useState([
     { rowNumber: 1, rowDescription: "Feed puppy", rowAssigned: "User one" },
     { rowNumber: 2, rowDescription: "Water plants", rowAssigned: "User two" },
@@ -15,26 +17,42 @@ function App() {
   ]);
 
   const addTodo = (description, assigned) => {
+    let rowNumber = 0;
+    if (todos.length > 0) {
+      rowNumber = todos[todos.length - 1].rowNumber + 1;
+    } else {
+      rowNumber = 1;
+    }
     if (todos.length > 0) {
       const newTodo = {
-        rowNumber: todos.length + 1,
+        rowNumber: rowNumber,
         rowDescription: description,
         rowAssigned: assigned,
       };
       setTodos((todos) => [...todos, newTodo]);
     }
   };
+
+  const deleteTodo = (deleteTodoRowNumber) => {
+    let filtered = todos.filter(function (value) {
+      return value.rowNumber !== deleteTodoRowNumber;
+    });
+
+    setTodos(filtered);
+  };
   return (
     <div className="mt-5 container">
       <div className="card">
         <div className="card-header">Your Todo's</div>
         <div className="card-body">
-          <TodoTable todos={todos} />
-          <button className="btn btn-primary" onClick={addTodo}>
-            Add new todo
+          <TodoTable todos={todos} deleteTodo={deleteTodo} />
+          <button
+            className="btn btn-primary"
+            onClick={() => setShowAddTodoForm(!showAddTodoForm)}
+          >
+            {showAddTodoForm ? "Close New Todo" : "New Todo"}
           </button>
-
-          <NewTodoForm addTodo={addTodo} />
+          {showAddTodoForm && <NewTodoForm addTodo={addTodo} />}
         </div>
       </div>
     </div>
--- src/components/TodoTable.js
+++ src/components/TodoTable.js
@@ -4,16 +4,20 @@ function TodoTable(props) {
   return (
     <table className="table table-hover">
       <thead>
-        <th scope="col">#</th>
-        <th scope="col">Description</th>
-        <th scope="col">Assigned</th>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Description</th>
+          <th scope="col">Assigned</th>
+        </tr>
       </thead>
       <tbody>
         {props.todos.map((todo) => (
           <TodoRowItem
+            key={todo.rowNumber}
             rowNumber={todo.rowNumber}
             rowDescription={todo.rowDescription}
             rowAssigned={todo.rowAssigned}
+            deleteTodo={props.deleteTodo}
           />
         ))}
       </tbody>
